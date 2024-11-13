2992adfd529a53ce68fec20cf61acb983539df1b
prafullohgaon <prafulkedari44@gmail.com>
2024-11-13 14:34:46 +0100

    Update apiConfig.js

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -2,12 +2,13 @@ import axios from "axios"
 
 export const API_BASE_URL="https://prafullohgaon-api.vercel.app"
 
-const jwt = localStorage.getItem("jwt")
+const jwt = localStorage.getItem("jwt") || ""; // Make sure there's a fallback to an empty string if jwt is not found
 
-export const api=axios.create({
-    baseURL:API_BASE_URL,
-    headers:{
+export const api = axios.create({
+    baseURL: API_BASE_URL,
+    headers: {
         "Authorization": `Bearer ${jwt}`,
-        'Content-Type':"application/json"
+        'Content-Type': "application/json"
     }
-})
+});
+
