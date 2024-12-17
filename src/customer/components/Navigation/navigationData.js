44a7552bfa3a2f1e620bc5b08152544a4b300ba4
prafullohgaon <prafulkedari44@gmail.com>
2024-12-17 20:26:58 +0100

    Update navigationData.js
    removed women section

--- src/customer/components/Navigation/navigationData.js
+++ src/customer/components/Navigation/navigationData.js
@@ -1,65 +1,6 @@
 export const navigation = {
     categories: [
       {
-        id: 'women',
-        name: 'Women',
-        featured: [
-          {
-            name: 'New Arrivals',
-            href: '/',
-            imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg',
-            imageAlt: 'Models sitting back to back, wearing Basic Tee in black and bone.',
-          },
-          {
-            name: 'Basic Tees',
-            href: '/',
-            imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg',
-            imageAlt: 'Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.',
-          },
-        ],
-        sections: [
-          {
-            id: 'clothing',
-            name: 'Clothing',
-            items: [
-              { name: 'Tops', id:"top", href: `{women/clothing/tops}` },
-              { name: 'Dresses', id:"women_dress", href: '#' },
-              { name: 'Women Jeans', id: 'women_jeans' },
-              { name: 'Lengha Choli', id: 'lengha_choli' },
-              { name: 'Sweaters', id: 'sweater' },
-              { name: 'T-Shirts', id: 't-shirt' },
-              { name: 'Jackets', id: 'jacket' },
-              { name: 'Gouns', id: 'gouns' },
-              { name: 'Sarees', id: 'saree' },
-              { name: 'Kurtas', id: 'kurtas' },
-            ],
-          },
-          {
-            id: 'accessories',
-            name: 'Accessories',
-            items: [
-              { name: 'Watches', id: 'watch' },
-              { name: 'Wallets', id: 'wallet' },
-              { name: 'Bags', id: 'bag' },
-              { name: 'Sunglasses', id: 'sunglasse' },
-              { name: 'Hats', id: 'hat' },
-              { name: 'Belts', id: 'belt' },
-            ],
-          },
-          // {
-          //   id: 'brands',
-          //   name: 'Brands',
-          //   items: [
-          //     { name: 'Full Nelson', id: '#' },
-          //     { name: 'My Way', id: '#' },
-          //     { name: 'Re-Arranged', id: '#' },
-          //     { name: 'Counterfeit', id: '#' },
-          //     { name: 'Significant Other', id: '#' },
-          //   ],
-          // },
-        ],
-      },
-      {
         id: 'men',
         name: 'Men',
         featured: [
